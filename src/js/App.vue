e0c8cbb195a810c4f546b27782aace3ca48b8e3c
deivan <ariastation@ua.fm>
2018-03-15 09:15:13 +0200

    Fixed advertise block

--- src/js/App.vue
+++ src/js/App.vue
@@ -34,8 +34,8 @@
     </div>
     <charts :data="products"></charts>
     <div class="main__charts--info">Щоб дивитись графіки, поверніть прилад горізонтально.</div>
-	<div style="width:100%;text-align:center;margin-top:10px">
-		<a href="https://hashflare.io/r/C6272D3B"><img src="https://cdn.hashflare.eu/banners/ru/profit_728x90_ru.gif?v=2" alt="HashFlare"></a>
+    <div class="main__advertise">
+      <a href="https://hashflare.io/r/C6272D3B"><img src="https://cdn.hashflare.eu/banners/ru/profit_728x90_ru.gif?v=2" alt="HashFlare"></a>
 	</div>
   </div>
 </template>
