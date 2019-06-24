6785b16d6e4fe57533bfe44a84c403b0d341d58c
Ivan <idemenkov@archer-soft.com>
2019-06-24 22:11:56 +0300

    updated SEO

--- src/js/App.vue
+++ src/js/App.vue
@@ -5,7 +5,7 @@
     <div class="fb-share-button" data-href="http://vegetables.dp.ua/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fvegetables.dp.ua%2F&amp;src=sdkpreparse">Поширити</a></div>
     <div class="main__block">
       <h2>Актуальні ціни на {{ currentDate|dates }}</h2>
-      <div class="main__block--content">
+      <div class="main__block--content"  title="Актуальная цена на овощи в Днепре">
         <div class="main__block--column">
           <p v-for="(item, index) in lastPrices" v-if="index < 7">{{item.name}} <span>&#8372; {{item.price|decimal}}</span></p>
         </div>
@@ -15,7 +15,7 @@
       </div>
     </div>
     <div class="main__block">
-      <h2>Овочевий калкулятор</h2>
+      <h2 title="Посчитать почем овощи Днепр">Овочевий калкулятор</h2>
         <div class="main__block--content">
           <div class="main__block--column">
             <div class="main__selector">
@@ -34,9 +34,9 @@
     </div>
     <charts :data="products"></charts>
     <div class="main__charts--info">Щоб дивитись графіки, поверніть прилад горізонтально.</div>
-    <div class="main__advertise">
+    <div class="main__advertise" title="овочі ціна Дніпро купити лол кєк чебурєк">
 <!--      <a href="https://hashflare.io/r/C6272D3B"><img src="https://cdn.hashflare.eu/banners/ru/profit_728x90_ru.gif?v=2" alt="HashFlare"></a>-->
-	</div>
+    </div>
   </div>
 </template>
 
